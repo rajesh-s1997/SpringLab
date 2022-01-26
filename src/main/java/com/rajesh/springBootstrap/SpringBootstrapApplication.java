a7438311be3972cdac38588e9cb3c27df9e99839
rajesh-s1997 <rajeshin1997@gmail.com>
2022-01-26 18:37:56 +0530

    initial commit

---
+++ src/main/java/com/rajesh/springBootstrap/SpringBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.rajesh.springBootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajesh/springBootstrap/SpringBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajesh.springBootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
